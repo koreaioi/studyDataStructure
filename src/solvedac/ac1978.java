b15a7bec64b6de67234765a8214a381461e72a60
koreaioi <koreaioi@gachon.ac.kr>
2023-12-13 03:07:14 +0900

    백준 1978번 문제풀이

--- src/solvedac/ac1978.java
+++ src/solvedac/ac1978.java
@@ -6,7 +6,7 @@ public class ac1978 {
     public static boolean isPrime(int n) {
         if(n==1) return false;
         if(n == 2) return true;
-        //i * i < n 도 가능
+        //i * i < n 도 가능하다
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if(n % i == 0) return false;
         }
