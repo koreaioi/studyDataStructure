5fa0c09a0c4182277a83d17207ea69cd842e1263
koreaioi <koreaioi@gachon.ac.kr>
2023-12-15 02:26:35 +0900

    12/15일

--- src/solvedac/ac2609.java
+++ src/solvedac/ac2609.java
@@ -4,9 +4,8 @@
 
     public class ac2609 {
         public static int gcd(int a, int b) {
-            int r =0;
             while (b != 0) {
-                r = a % b;
+                int r = a % b;
                 a = b;
                 b = r;
             }
@@ -14,13 +13,10 @@
         }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            int n = 2; //최대 공약수
-            int m = 0; //최소 공배수
-            int r=0;
             int a = sc.nextInt();
             int b = sc.nextInt();
 
-            r = gcd(a, b);
+            int r = gcd(a, b);
             System.out.println(r);
             System.out.println(a * b / r);
         }
