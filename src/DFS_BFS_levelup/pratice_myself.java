7291a855afd7526a7cfa872b77c0b70a4eb6fc1e
koreaioi <koreaioi@gachon.ac.kr>
2024-02-03 22:59:45 +0900

    연습

--- src/DFS_BFS_levelup/pratice_myself.java
+++ src/DFS_BFS_levelup/pratice_myself.java
@@ -1,34 +1,29 @@
 package DFS_BFS_levelup;
 import java.util.*;
 
-
 public class pratice_myself {
-    public static int n; //문제 수
-    public static int limit; //제한 시간
-    public static int[][] arr; //문제와 푸는 시간을 담은 배열
-    public static int answer=0;//답
+    public static int n,m;
+    public static int[] arr;
 
-    public static void DFS(int L, int sum, int time) {
-        if(time > limit) return;
-        if(L==n)answer = Math.max(answer, sum);
-        else{
-            DFS(L + 1, sum + arr[L][0], time + arr[L][1]);
-            DFS(L + 1, sum, time );
+    public static void DFS(int L) {
+        if (L == m) {
+            for(int x : arr) System.out.print(x + " ");
+            System.out.println();
+        }else {
+            for (int i = 1; i <= n; i++) {
+                arr[L] = i;
+                DFS(L + 1);
+            }
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        limit = sc.nextInt();
-        arr = new int[n][2]; //[][0]이 문제 [][1]이 시간
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
+        m = sc.nextInt();
+        arr = new int[m];
+        DFS(0);
 
-        DFS(0, 0, 0);
-        System.out.println(answer);
     }
 }
+
