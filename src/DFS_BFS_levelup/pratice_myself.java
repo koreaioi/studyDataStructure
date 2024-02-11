262db23c64182bb0d0996b93117591c6215c18a0
koreaioi <koreaioi@gachon.ac.kr>
2024-02-11 15:14:40 +0900

    .

--- src/DFS_BFS_levelup/pratice_myself.java
+++ src/DFS_BFS_levelup/pratice_myself.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class pratice_myself {
     public static int n,m;
-    public static int[] arr;
-
-    public static void DFS(int L) {
-        if (L == m) {
-            for(int x : arr) System.out.print(x + " ");
-            System.out.println();
-        }else {
-            for (int i = 1; i <= n; i++) {
-                arr[L] = i;
-                DFS(L + 1);
+    public static Integer[] arr;
+    public static int answer = Integer.MAX_VALUE;
+    public static void DFS(int L, int sum) {
+        if(sum > m) return;
+        if(L>= answer) return;
+        if(sum == m){
+            answer = Math.min(answer, L);
+        }else if (sum < m){
+            for (int i = 0; i < n; i++) {
+                DFS(L + 1, sum + arr[i]);
             }
         }
     }
@@ -20,10 +20,12 @@ public class pratice_myself {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
+        arr = new Integer[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
         m = sc.nextInt();
-        arr = new int[m];
-        DFS(0);
-
+        Arrays.sort(arr, Collections.reverseOrder());
+        DFS(0, 0);
+        System.out.println(answer);
     }
 }
 
