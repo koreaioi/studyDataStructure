680b7a20f62a925a05b9bc8ff13fe24a64e7c91f
koreaioi <koreaioi@gachon.ac.kr>
2024-03-05 09:33:57 +0900

    백준 7576번

--- src/solvedac/ac7576.java
+++ src/solvedac/ac7576.java
@@ -3,18 +3,30 @@ import java.util.*;
 
 public class ac7576 {
     static int[][] board, ch;
-    static int m, n;
+    static int m, n,count;
     static Queue<Point> q = new LinkedList<>();
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, -1, 0, 1};
 
     public static void BFS() {
-
-
+        count =0;
         while(!q.isEmpty()){
+            int L = q.size();
 
+            for (int j = 0; j < L; j++) {
+            Point tmp = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = tmp.x + dx[i];
+                    int ny = tmp.y + dy[i];
+                    if (0 <= nx && nx < n && 0 <= ny && ny < m && board[nx][ny] == 0 && ch[nx][ny] == 0) {
+                        q.add(new Point(nx, ny));
+                        board[nx][ny] = 1;
+                        ch[nx][ny] = 1;
+                    }
+                }
+            }
+            count++;
         }
-
     }
 
     public static void main(String[] args) {
@@ -34,21 +46,17 @@ public class ac7576 {
             }
         }
 
-
-
-
-
-/*
-        //test
+        BFS();
         for (int[] arr : board) {
             for (int x : arr) {
-                System.out.print(x + " ");
+                if(x==0){
+                    System.out.println(-1);
+                    return;
+                }
             }
-            System.out.println();
         }
+        System.out.println(count-1);
 
-
-*/
 
 
     }
